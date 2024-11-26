f0168e6a23a64a5b8fd306abd1f6e3bd63f059b7
Fábio P Silva <fabiopereira.fps@gmail.com>
2024-11-25 23:00:41 -0300

    add api

---
+++ newsletter-api/src/test/java/com/github/fabioper/newsletterapi/NewsletterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabioper.newsletterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsletterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
